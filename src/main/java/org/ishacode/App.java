6e22dea2948211154004098db3f008b9b299276a
itsdev04 <imkrdev@gmail.com>
2025-07-28 14:52:35 +0530

    test commit

--- src/main/java/org/ishacode/App.java
+++ src/main/java/org/ishacode/App.java
@@ -6,6 +6,7 @@ package org.ishacode;
  */
 public class App 
 {
+    /*Test comment to see the commit*/
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
