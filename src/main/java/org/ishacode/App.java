f1b99cd9ec950f0d718c9063e762caccacc80c90
itsdev04 <imkrdev@gmail.com>
2025-07-28 14:44:17 +0530

    code refactor

--- src/main/java/org/ishacode/App.java
+++ src/main/java/org/ishacode/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.ishacode;
 
 /**
  * Hello world!
--- src/test/java/org/ishacode/AppTest.java
+++ src/test/java/org/ishacode/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.ishacode;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
