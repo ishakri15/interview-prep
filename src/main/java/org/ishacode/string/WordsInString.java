02a4e775f6fbc23646f1d78600dc513aa6f8a7e1
itsdev04 <imkrdev@gmail.com>
2025-08-17 23:46:06 +0530

    character array concept

--- src/main/java/org/ishacode/string/WordsInString.java
+++ src/main/java/org/ishacode/string/WordsInString.java
@@ -2,7 +2,7 @@ package org.ishacode.string;
 
 public class WordsInString {
     public static void main(String[] args) {
-        String s = "Welcome to java";
+        String s = "Welcome  to java";
         int count = 1;
         for(int i=0; i<s.length()-1; i++){
             if(s.charAt(i)==' ' && s.charAt(i+1)!=' '){
