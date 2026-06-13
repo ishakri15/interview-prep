f9c6fb733553adbc7be65081759d823a73f9bb37
itsdev04 <imkrdev@gmail.com>
2026-06-14 01:44:57 +0530

    group anagram

--- src/main/java/org/devworks/string/FirstCap.java
+++ src/main/java/org/devworks/string/FirstCap.java
@@ -10,7 +10,6 @@ public class FirstCap {
     Arrays.stream(arr)
         .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
         .collect(Collectors.toList())
-        .forEach(
-                System.out::println);
+        .forEach(System.out::println);
   }
 }
