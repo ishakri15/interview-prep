308c0601a20ebf4e15120110676181faa81f8e90
itsdev04 <imkrdev@gmail.com>
2026-06-16 00:01:56 +0530

    anagram

--- src/main/java/org/devworks/sdesheet/arraystring/GroupAnagramTogetherBySorting.java
+++ src/main/java/org/devworks/sdesheet/arraystring/GroupAnagramTogetherBySorting.java
@@ -1,51 +1,42 @@
 package org.devworks.sdesheet.arraystring;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class GroupAnagramTogetherBySorting {
 
-    private String sort(String s){
-       char[] chars = s.toCharArray();
-        for(int i =0; i<chars.length;i++){
-            char c =
-            for(int j=i+1; j<chars.length; j++){
-                if
-            }
-        }
+  private static String sort(String s) {
+    char[] chars = s.toCharArray();
+    Arrays.sort(chars);
+    return new String(chars);
+  }
+
+  static List<List<String>> groupAnagramCategorizedBySorting(String[] strs) {
+
+    if (strs == null || strs.length == 0) return new ArrayList<>();
+
+    Map<String, List<String>> sortedStringHashMap = new HashMap<>();
+
+    for (String str : strs) {
+      String sortedString = sort(str);
+      if (sortedStringHashMap.containsKey(sortedString)) {
+        sortedStringHashMap.get(sortedString).add(str);
+      } else {
+        List<String> stringList = new ArrayList<>();
+        stringList.add(sortedString);
+        sortedStringHashMap.put(sortedString, stringList);
+      }
     }
 
-    static void groupAnagramCategorizedBySorting(String [] strs){
-        //if(strs == null || strs.length==0) return new ArrayList<>();
-
-
-
-        List<String> sortedStrings = Arrays.stream(strs).sorted().collect(Collectors.toList());
-
-        System.out.println("Sorted String");
-        sortedStrings.forEach(System.out::println);
-
-        HashMap<String, List<String>> sortedStringHashMap = new HashMap<>();
-
-        for(String s:sortedStrings){
-            if(sortedStrings.contains(s)){
-                sortedStringHashMap.put(s,sortedStrings);
-            }else{
-                //create a new list
-                List<String> stringList = new ArrayList<>();
-                stringList.add(s);
-                sortedStringHashMap.put(s,stringList);
-            }
-        }
-
-        for(Map.Entry<String,List<String>> entry: sortedStringHashMap.entrySet()){
-            System.out.println(entry.getKey() +"->"+ entry.getValue());
-        }
+    for (Map.Entry<String, List<String>> entry : sortedStringHashMap.entrySet()) {
+      System.out.println(entry.getKey() + "->" + entry.getValue());
     }
 
-    public static void main(String[] args) {
-        String[] strs = {"act", "god", "cat", "dog", "tac"};
-         groupAnagramCategorizedBySorting(strs);
-         }
+    return new ArrayList<>(sortedStringHashMap.values());
+  }
+
+  public static void main(String[] args) {
+    String[] strs = {"act", "god", "cat", "dog", "tac"};
+    List<List<String>> res = groupAnagramCategorizedBySorting(strs);
+    System.out.print(res);
+  }
 }
